fba654611697ee755f725949e7970cccaa34e1a2
maydindev <maydin.dev@outlook.com>
2024-09-24 11:18:11 +0300

    "Updated login page styling and removed margin-left from input fields and buttons"

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,26 @@ export default function Home() {
       <div>
         <div className="flex justify-center content-center">
           <div className="mb-12 content-end">
-            <img src="/login.png" alt="" className="w-36 h-10 mb-5" />
+            {/*<img src="/login.png" alt="" className="w-36 h-10 mb-5" />*/}
+            {/*<p className="font-montserrat text-red-600 text-5xl mb-5">LOGIN</p>*/}
+            <p style={{fontFamily:"futura", fontSize:"50px", fontWeight:"bolder", color:"transparent",WebkitTextStroke: "1px red"}}>LOGIN</p>
             <input
               type="text"
               placeholder="Username"
-              className="border border-grey-900 p-2 mb-2 w-72 h-8 ml-1"
+              className="border border-grey-900 p-2 mb-2 w-72 h-8"
             />
             <br />
             <input
               type="text"
               placeholder="Password"
-              className="border border-grey-900 p-2 mb-5 w-72 h-8 ml-1"
+              className="border border-grey-900 p-2 mb-5 w-72 h-8"
             />
             <br />
-            <button className="bg-red-500 w-48 h-10 ml-1 text-white font-bold mb-2">
+            <button className="bg-red-500 w-48 h-10 text-white font-bold mb-2">
               LOGIN
             </button>
             <br />
-            <button className="mr-40 ml-1 font-bold">Forgot Password</button>
+            <button className="mr-40 font-bold">Forgot Password</button>
             <button className="mr-10 font-semibold">Register</button>
           </div>
 
